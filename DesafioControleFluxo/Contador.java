7490fd6b54c89fae797e9a9c5208d95ac900cd92
sergiolucamargo <153140172+sergiolucamargo@users.noreply.github.com>
2025-02-24 00:10:00 -0300

    Implementação Classe Contador.java e Exceção ParametrosInvalidosException.java

--- DesafioControleFluxo/Contador.java
+++ DesafioControleFluxo/Contador.java
@@ -1,3 +1,33 @@
-public class Contador {
+import java.util.Scanner;
 
+public class Contador
+{
+    public static void main(String[] args) throws ParametrosInvalidosException
+  {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = sc.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = sc.nextInt();
+
+        contar(parametroUm, parametroDois);
+
+        sc.close();
+    }
+  
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException
+    {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i=1; i<=contagem; i++)
+        {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
 }
